ed31eda271370d487193cca2953efe44888a661b
layken varholdt <laykenv@gmail.com>
2023-04-25 13:16:02 -0500

    updated to hashrouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ Amplify.configure(AMPLIFY_CONFIG);
 import "./styles.css";
 import LandingPage from './pages/LandingPage';
 import  ProfilePage  from './pages/ProfilePage'
-import { BrowserRouter, RouterProvider, Routes, Route, useLocation, useNavigate} from 'react-router-dom';
+import { BrowserRouter, RouterProvider, Routes, Route, useLocation, useNavigate, HashRouter} from 'react-router-dom';
 import { RequireAuth } from './components/RequireAuth';
 import { Login } from './components/Login';
 import { DataProvider } from './dataContext';
@@ -27,14 +27,14 @@ const App = () => {
   return (
     
     <DataProvider>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<LandingPage><Login></Login></LandingPage>}></Route>
         <Route path='/profile' element={<RequireAuth><div style = {{display: 'flex', flexDirection: 'row'}}><NavBar></NavBar><ProfilePage></ProfilePage></div></RequireAuth>}></Route>
         <Route path= '/dashboard' element = {<RequireAuth><div style = {{display: 'flex', flexDirection: 'row'}}><NavBar></NavBar><Dashboard></Dashboard></div></RequireAuth>}/>
         <Route path= '/usermanagement' element = {<RequireAuth><div style = {{display: 'flex', flexDirection: 'row'}}><NavBar></NavBar><div>User Management!!!</div></div></RequireAuth>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </DataProvider>
   )
 
